d5599617940aa691697f1e414af3438b5a1a750a
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2025-11-19 15:46:34 +0700

    addAnwser

--- task02.ts
+++ task02.ts
@@ -1,5 +1,14 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = 18 ;
 // Try changing to "hello", 10, null, NaN
+// if (true) {
+//     console.log(`Your in put: ${userInput} || type is ${typeof(userInput)} || ${Boolean(userInput)}`);
+// }
+//isNaN เป็นการตั้งคำถามว่าเป็น NaN หรือไม่และคำตอบที่ได้จาก is จะเป็น True and False เท่านั้น
+if (typeof(userInput) === 'number' && !isNaN(userInput)){
+    console.log(`This in put : ${userInput} is number `)
+} else {
+    console.log(`This in put : ${userInput} is NOT a number and this type is ${typeof(userInput)}`)
+}
